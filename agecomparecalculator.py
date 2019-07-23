22423b77189963fd204a1e60e167ad9682847f3b
leen21-meet <leen21@meet.mit.edu>
2019-07-23 03:32:56 -0400

    Stopping here

---
+++ agecomparecalculator.py
@@ -0,0 +1,10 @@
+person_one = input ("what is the age of the first person ?")
+person_two = input ("what is the age of the first person ?")
+person_one =int (person_one )
+person_two =int (person_two)
+comp = person_one - person_two
+comp = abs (comp)
+print (comp)
+
+
+            
